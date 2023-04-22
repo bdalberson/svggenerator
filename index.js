e76a92d7808fe95196ff0645ab22057256eac917
bdalberson <Brian.alberson@gmail.com>
2023-04-22 10:55:35 -0700

    tutor session built cases

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const inquirer = require('inquirer')
+const Circle = require("./lib/Circle")
+const Triangle = require("./lib/Triangle")
+const Square = require("./lib/Square")
 
-class Shapes {}
+function writeSVGfile(filename="", data){
+    fs.writeFile("shape.svg",data ).then((result) => {
+        console.log(result)
+    }).catch((err)=> console.log(err))
 
-class Triannge {}     //should extend Shapes
+}
 
-class Circle {}       //should extend Shapes
+function startPrompt() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'What text would you like?',
+          validate: function (input) { 
+            return input.length > 1
+          }
+        },
+        {
+          type: 'input',
+          message: 'What colow would you like? Hex accepted',
+          name: 'color',
+          validate: function (input) { 
+            return input.length > 1
+        }
+        },
+        {
+          type: 'input',
+          message: 'What shape would you like?',
+          name: 'shape',
+          validate: function (input) { 
+            return input.length > 3
+          }
+        }
+    ]).then((data) => {
+        console.log(data)
+        switch (data.shape) {
+            case "circle":
+                const circle = new Circle(data.color,data.text)
+                writeSVGfile("circle", circle.render())
+                
+                break;
+            case "square":
+                const square = new Square(data.color,data.text)
 
-class Square {}       //should extend Shapes
+                break;
+            case "triangle":
+                const triange = new Triangle(data.color,data.text)
 
+                break;
+        
+            default:
+                return null 
+        }
+    }).catch((err) => {
+        process.exitCode = 1
+        if (err) throw err;
+        console.log('The file could not be saved!');
+        process.exit()
+    })
+}
 
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'What text would you like?',
-      validate: function (input) { 
-        return input.length > 1
-      }
-    },
-    {
-      type: 'input',
-      message: 'What colow would you like? Hex accepted',
-      name: 'color',
-      validate: function (input) { 
-        return input.length > 1
-    }
-    },
-    {
-      type: 'input',
-      message: 'What shape would you like?',
-      name: 'title',
-      validate: function (input) { 
-        return input.length > 3
-      }
-    }
-]).then((data) => {
-    console.log(data)
-},(err) => {
-    if (err) throw err;
-    console.log('The file could not be saved!');
-})
+startPrompt()
