55d77883f63b456b31af15fd59b085efe16994ea
bdalberson <Brian.alberson@gmail.com>
2023-04-22 09:48:26 -0700

    Added inquierer prompts

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'What text would you like?',
+      validate: function (input) { 
+        return input.length > 3
+      }
+    },
+    {
+      type: 'input',
+      message: 'What colow would you like? Hex accepted',
+      name: 'color',
+      validate: function (input) { 
+        return input.length > 3
+    }
+    },
+    {
+      type: 'input',
+      message: 'What shape would you like?',
+      name: 'title',
+      validate: function (input) { 
+        return input.length > 3
+      }
+    }
+]).then((data) => {
+    console.log(data)
+},(err) => {
+    if (err) throw err;
+    console.log('The file could not be saved!');
+})
