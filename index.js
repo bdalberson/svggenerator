10981815190ceec8b8366a9978debf2bc9177f09
bdalberson <Brian.alberson@gmail.com>
2023-04-22 11:55:30 -0700

    fixing inquier adding class definitions

--- index.js
+++ index.js
@@ -4,66 +4,64 @@ const Circle = require("./lib/Circle")
 const Triangle = require("./lib/Triangle")
 const Square = require("./lib/Square")
 
-function writeSVGfile(filename="", data){
-    fs.writeFile("shape.svg",data ).then((result) => {
+function writeSVGfile(filename = "", data) {
+    fs.writeFile("shape.svg", data).then((result) => {
         console.log(result)
-    }).catch((err)=> console.log(err))
+    }).catch((err) => console.log(err))
 
 }
 
 function startPrompt() {
     inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'What text would you like?',
-          validate: function (input) { 
-            return input.length > 1
-          }
-        },
-        {
-          type: 'input',
-          message: 'What colow would you like? Hex accepted',
-          name: 'color',
-          validate: function (input) { 
-            return input.length > 1
-        }
-        },
-        {
-          type: 'input',
-          message: 'What shape would you like?',
-          name: 'shape',
-          validate: function (input) { 
-            return input.length > 3
-          }
-        }
-    ]).then((data) => {
-        console.log(data)
-        switch (data.shape) {
-            case "circle":
-                const circle = new Circle(data.color,data.text)
-                writeSVGfile("circle", circle.render())
-                
-                break;
-            case "square":
-                const square = new Square(data.color,data.text)
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'What text would you like?',
+                validate: function (input) {
+                    return input.length > 1
+                }
+            },
+            {
+                type: 'input',
+                message: 'What colow would you like? Hex accepted',
+                name: 'color',
+                validate: function (input) {
+                    return input.length > 1
+                }
+            },
+            {
+                type: 'checkbox',
+                message: 'What shape would you like?',
+                name: 'shape',
+                choices: ['circle', 'triangle', 'square']
+            },
+            ]).then((data) => {
+                console.log(data)
+                switch (data.shape) {
+                    case "circle":
+                        const circle = new Circle(data.color, data.text)
+                        writeSVGfile("circle", circle.render())
 
-                break;
-            case "triangle":
-                const triange = new Triangle(data.color,data.text)
+                        break;
+                    case "square":
+                        const square = new Square(data.color, data.text)
 
-                break;
-        
-            default:
-                return null 
-        }
-    }).catch((err) => {
-        process.exitCode = 1
-        if (err) throw err;
-        console.log('The file could not be saved!');
-        process.exit()
-    })
+                        break;
+                    case "triangle":
+                        const triange = new Triangle(data.color, data.text)
+
+                        break;
+
+                    default:
+                        return null
+                }
+            }).catch((err) => {
+                process.exitCode = 1
+                if (err) throw err;
+                console.log('The file could not be saved!');
+                process.exit()
+            })
 }
 
 startPrompt()
